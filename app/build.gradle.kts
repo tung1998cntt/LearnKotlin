cdfa616334e5ab0037eb1427cbbb6118fd9cdd2e
hodientung <hodientung1998cntt@gmail.com>
2026-07-20 17:43:21 +0700

    [KOTLIN]: Add suribus

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.example.learnkotlin"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
